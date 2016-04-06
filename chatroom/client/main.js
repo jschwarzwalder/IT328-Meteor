a0e93948fdee6d6b507f1ac848821c3786d32ef3
Jami <jamischwarzwalder@gmail.com>
2016-04-05 20:51:25 -0700

    End of class ChatApp

--- chatroom/client/main.js
+++ chatroom/client/main.js
@@ -5,6 +5,17 @@ import './main.html';
 //we need some initial data to work with
 Template.messages.onCreated(function messagesOnCreate(){
 	//create some initial data and store it in session
-	Session.setDefault()
-	
+	Session.setDefault("messagesData, [
+		{poster: "Arnold", message:"Hello IT 328"},
+		{poster:"Jami", message:"Javascript is crazzzzy!"},
+		{poster: "Josh", message:"Too many Joshs...."}
+	]")
 });
+
+Template.messages.helpers({
+	getMessages: function () {
+		return Session.get("messagesData";)
+	}
+});
+
+
