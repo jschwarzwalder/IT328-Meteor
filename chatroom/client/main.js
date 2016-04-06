a8033ba1a6e3985b0a7da24713c4d8586c165a01
Jami <jamischwarzwalder@gmail.com>
2016-04-05 23:29:24 -0700

    adding more to the Hello World app

--- chatroom/client/main.js
+++ chatroom/client/main.js
@@ -1,22 +1,23 @@
 import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
 //we need some initial data to work with
-Template.messages.onCreated(function messagesOnCreated(){
+Template.messages.onCreated(function (){
 	//create some initial data and store it in session
 	Session.setDefault("messagesData", [
 		{poster: "Arnold", message:"Hello IT 328"},
-		{poster:"Jami", message:"Javascript is crazzzzy!"},
-		{poster: "Josh", message:"Too many Joshs...."}
+		{poster: "Jami", message:"Javascript is crazzzzy!"},
+		{poster: "Josh", message:"Too many Joshes...."},
+		{poster: "Paul", message:"Shouldn't there be an e"},
+		{poster: "Paul", message:"Shouldn't there be quotes around that \'e'"},
+		{poster: "Myriad", message:"MeeeeeeOOOW"}
 	]);
 });
 
-var messagesHelper = {
-	getMessages = function(){
-	return Session.get("messagesData");
-	};
-};
-Template.messages.helpers(messagesHelper);
-
-
+Template.messages.helpers({
+	getMessages: function () {
+		return Session.get("messagesData");
+	}
+});
