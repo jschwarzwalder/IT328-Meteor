5242f27a58d73710a6479968f3c3f30d74825ae2
Jami <jamischwarzwalder@gmail.com>
2016-04-05 20:56:39 -0700

    Working Chat room

--- chatroom/client/main.js
+++ chatroom/client/main.js
@@ -3,18 +3,18 @@ import { Template } from 'meteor/templating';
 import './main.html';
 
 //we need some initial data to work with
-Template.messages.onCreated(function messagesOnCreate(){
+Template.messages.onCreated(function messagesOnCreated(){
 	//create some initial data and store it in session
-	Session.setDefault("messagesData, [
+	Session.setDefault("messagesData", [
 		{poster: "Arnold", message:"Hello IT 328"},
 		{poster:"Jami", message:"Javascript is crazzzzy!"},
 		{poster: "Josh", message:"Too many Joshs...."}
-	]")
+	]);
 });
 
 Template.messages.helpers({
 	getMessages: function () {
-		return Session.get("messagesData";)
+		return Session.get("messagesData");
 	}
 });
 
