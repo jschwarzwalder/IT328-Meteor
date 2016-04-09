d619138a0e947b3d923619b108ffdf64d7e188bf
Jami <jamischwarzwalder@gmail.com>
2016-04-09 00:04:31 -0700

    getting rid of the if on client function

--- myFridge/client/main.js
+++ myFridge/client/main.js
@@ -8,40 +8,39 @@
  
 Products = new Mongo.Collection('products');
 
-if (Meteor.isClient) {
-	Template.fridge.onRendered(function () {
-		var templateInstance = this;
 
-		templateInstance.$('#fridge').droppable({
-		  drop: function (evt, ui) {
-			var query = {
-			  _id: ui.draggable.data('id')
-			};
-			var changes = {
-			  $set: {
-				place: 'fridge'
-			  }
-			};
-			Products.update(query, changes);
+Template.fridge.onRendered(function () {
+	var templateInstance = this;
+
+	templateInstance.$('#fridge').droppable({
+	  drop: function (evt, ui) {
+		var query = {
+		  _id: ui.draggable.data('id')
+		};
+		var changes = {
+		  $set: {
+			place: 'fridge'
 		  }
-		});
+		};
+		Products.update(query, changes);
+	  }
 	});
-	
-	Template.fridge.helpers({
-		 products: function () {
-			 return Products.find({
-				place: 'fridge'
-			 });
-		 }
-	});
-	Template.productList.helpers({
-		 products: function () {
-			 return Products.find({
-				place: 'supermarket'
-			 });
-		 }
-	});
-}
+});
+
+Template.fridge.helpers({
+	 products: function () {
+		 return Products.find({
+			place: 'fridge'
+		 });
+	 }
+});
+Template.productList.helpers({
+	 products: function () {
+		 return Products.find({
+			place: 'supermarket'
+		 });
+	 }
+});
 
 Template.productList.onRendered(function () {
     var templateInstance = this;
