f3d9a5a91414184eb86aa5ffcdd73506c016e8bb
Jami <jamischwarzwalder@gmail.com>
2016-05-06 00:42:17 -0700

    Creating helper to make miniMongo on client

--- mongoPractice/client/main.js
+++ mongoPractice/client/main.js
@@ -1,5 +1,13 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+import { bookmarkCollection } from '../collection/collections.js';
+//tempoarily holds cache of files form server - miniMongo
 
 import './main.html';
 
+Template.bookmarks.helpers({
+	bookmarksList: function(){
+		//retreve all bookmarks from our collection
+		return bookmarkCollection.find();
+	}
+})
