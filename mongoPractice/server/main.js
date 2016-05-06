b11f8bcee25052bdc7e6bbfc8741f1006379619e
Jami <jamischwarzwalder@gmail.com>
2016-05-06 00:28:16 -0700

    adding DummyData to Collection on server

--- mongoPractice/server/main.js
+++ mongoPractice/server/main.js
@@ -3,5 +3,13 @@ import { bookmarkCollection } from '../collection/collections.js';
 import { bookmarksDummyData } from '../collection/collections.js';
 
 Meteor.startup(() => {
+	//remove any database values that are present
+	bookmarkCollection({}); //delete all records (this will only work on the server)
+		
+	//add my dummy values
+	for (var i=0; i < bookmarksDummyData.length; i++) {
+		bookmarkCollection.insert(bookmarksDummyData[i]);
+	}
+		
 	
 });
