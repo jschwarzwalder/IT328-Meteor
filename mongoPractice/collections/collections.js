711335d2f38a5d5a1ea4f1a6a4b1f0228cf97209
Jami <jamischwarzwalder@gmail.com>
2016-05-06 00:22:53 -0700

    adding dummy data

--- mongoPractice/collections/collections.js
+++ mongoPractice/collections/collections.js
@@ -5,3 +5,25 @@ import { Mongo} from "meteor/mongo";
 export const bookmarksCollection = new Mongo.Collection("bookmarks");
 
 //export access to any data to start our application
+export const bookmarksDummyData = [
+	{
+		"name": "Pierce County Library",
+		"url": "http://www.piercecountylibrary.org"
+		"lastVisited": new Date(2007, 5, 16)
+	},
+	{
+		"name": "Teen Summer Challenge",
+		"url": "http://beta.teensummerchallenge.org"
+		"lastVisited": new Date()
+	},
+	{
+		"name": "Ludum Dare",
+		"url": "http://ludumdare.com/compo/"
+		"lastVisited": new Date(2016, 4, 18)
+	},
+	{
+		"name": "Crash Course Games",
+		"url": "https://www.youtube.com/playlist?list=PL8dPuuaLjXtPTrc_yg73RghJEOdobAplG"
+		"lastVisited": new Date(2016, 4, 22)
+	},
+]
