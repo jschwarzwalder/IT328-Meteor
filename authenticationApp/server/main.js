3788fcf0543dedd7c624b7c8955ac216f1be690d
Jami <jamischwarzwalder@gmail.com>
2016-05-10 19:33:26 -0700

    publish data on serverside

--- authenticationApp/server/main.js
+++ authenticationApp/server/main.js
@@ -3,3 +3,9 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+//publish data that a client may or maynot want
+Meteor.publish('userData', function(){
+		
+	
+});
