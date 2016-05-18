69947b43b14732d92886352fa30cf9b1824ebf59
Jami <jamischwarzwalder@gmail.com>
2016-05-17 19:14:52 -0700

    Trying to make data visible in template

--- exerciseApplication/collections/collections.js
+++ exerciseApplication/collections/collections.js
@@ -1,6 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 
-export const activityCollection = new Mong.Collection('activities');
+export const activityCollection = new Mongo.Collection('activities');
 //mongo read one on the right
 //application reads name on the left
