0bf9aedc89a454e23556facbf49095db5ac87192
Jami <jamischwarzwalder@gmail.com>
2016-05-17 19:04:03 -0700

    building new events

--- exerciseApplication/client/main.js
+++ exerciseApplication/client/main.js
@@ -3,3 +3,16 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
+Template.newActivity.events ({
+	'submit #enterActivity': function(event){
+		//stop the form from submitting
+		event.preventDefault();
+		
+		//we need a JSON document
+		var newDocument = {
+			"type": $('#activityType').val(), 
+			"description": $('#activityDescription').val(), 
+			"hours": $('#activityHours').val()
+		}
+	}
+})
--- exerciseApplication/server/main.js
+++ exerciseApplication/server/main.js
@@ -1,5 +1,20 @@
 import { Meteor } from 'meteor/meteor';
+import { activityCollection } from '../collections/collections.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
+});
+
+//insert, update, and delete functionality through
+//secure Meteor methods
+Meteor.methods ({
+	activityInsert: function(activity){
+		activityCollection.insert(activity);
+	}, 
+	activityDelete: function(_id){
+		
+	},
+	activityUpdate: function(updatedActivity){
+		
+	}
 });
