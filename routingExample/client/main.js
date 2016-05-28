2b02f9af51e78dbb933a0e3fb0c6ae39df81718a
Jami <jamischwarzwalder@gmail.com>
2016-05-28 01:47:49 -0700

    Subscribing to all products and using it in a helper

--- routingExample/client/main.js
+++ routingExample/client/main.js
@@ -3,3 +3,13 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import { productsCollection } from '../collections/collections.js';
 
 import './main.html';
+
+//import my products collecation and make it available to a template
+
+Meteor.subscribe('allProducts');
+
+Template.productList.helpers({
+	products: function(){
+		return productsCollection.find();
+	}
+})
