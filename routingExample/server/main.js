423630670fa8c86ebff955f0aead61e8bfab0c63
Jami <jamischwarzwalder@gmail.com>
2016-05-28 01:09:58 -0700

    Server file from lecture video

--- routingExample/server/main.js
+++ routingExample/server/main.js
@@ -1,2 +1,28 @@
 import { Meteor } from 'meteor/meteor';
 import { productsCollection } from '../collections/collections.js';
+
+Meteor.startup(() => {
+	Meteor.call('addDummyData');
+});
+
+Meteor.methods({
+	productinsert: function(product) {
+		//return the id of the new record
+		return productsCollection.insert(product);
+	}, 
+	productDelete: function(_id){
+		productsCollection.remove({"_id": _id});
+	}, 
+	productUpdate: function(updatedProduct){
+		productsCollection.update({"_id": updatedProduct._id}, {"$set": {
+			name: updatedProduct.name,
+			price: updatedProduct.price,
+			description: updatedProduct.description
+			
+		}});
+	}
+})
+
+Meteor.publish('allProducts', function(){
+	return.productsCollection.find();
+});
