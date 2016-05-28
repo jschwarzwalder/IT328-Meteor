2f567d2a9a42e98e17b7eb24749b3ec7e01f2795
Jami <jamischwarzwalder@gmail.com>
2016-05-28 01:39:51 -0700

    adding a second "page"

--- routingExample/routes/routes.js
+++ routingExample/routes/routes.js
@@ -2,5 +2,9 @@ import {productsCollection } from '../collections/collections.js';
 
 //defind some routes (associate URLS with templates)
 Router.route('/', function(){
-	this.render('productList');//view allProducts template
+	this.render('productList');//view productList template
+});
+
+Router.route('/viewProduct', function(){
+	this.render('viewProduct');//view viewProduct template
 });
